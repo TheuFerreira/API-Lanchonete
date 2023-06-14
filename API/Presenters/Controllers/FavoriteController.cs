d7f27f7939218fa9b21a73e9068872d168f27ffa
Matheus Ferreira <matheus.44300@gmail.com>
2023-06-13 22:36:36 -0300

    Created FavoriteController

---
+++ API/Presenters/Controllers/FavoriteController.cs
@@ -0,0 +1,90 @@
+﻿using API.Presenters.Cases;
+using API.Presenters.Requests;
+using API.Presenters.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Presenters.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class FavoriteController : ControllerBase
+    {
+        private readonly IFavoriteProductCase favoriteProductCase;
+        private readonly ISearchFavoritesCase searchFavoritesCase;
+        private readonly ICountFavoritesCase countFavoritesCase;
+
+        public FavoriteController(
+            IFavoriteProductCase favoriteProductCase,
+            ISearchFavoritesCase searchFavoritesCase,
+            ICountFavoritesCase countFavoritesCase
+        )
+        {
+            this.favoriteProductCase = favoriteProductCase;
+            this.searchFavoritesCase = searchFavoritesCase;
+            this.countFavoritesCase = countFavoritesCase;
+        }
+
+        [HttpPut]
+        [Route("Favorite")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult UpdateFavorite(UpdateFavoriteRequest request)
+        {
+            int userId = 1; // TODO: Remove for Token
+
+            try
+            {
+                bool newValue = favoriteProductCase.Execute(userId, request.ProductId);
+                return Ok(newValue);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("Search")]
+        [ProducesResponseType(typeof(IEnumerable<SearchFavoritesResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult Search(SearchFavoritesRequest request)
+        {
+            int userId = 1;
+
+            try
+            {
+                IEnumerable<SearchFavoritesResponse> response = searchFavoritesCase.Execute(userId, request.Search);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("Count")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetCount()
+        {
+            int userId = 1;
+
+            try
+            {
+                int count = countFavoritesCase.Execute(userId);
+                return Ok(count);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
--- API/Presenters/Controllers/ProductController.cs
+++ API/Presenters/Controllers/ProductController.cs
@@ -14,25 +14,16 @@ namespace API.Presenters.Controllers
         private readonly IGetProductInfoCase getProductInfoCase;
         private readonly IGetAllProductsByCategoriesCase getAllProductsByCategoriesCase;
         private readonly IGetAllProductsBestSellersByCategoriesCase getAllProductsBestSellersByCategoriesCase;
-        private readonly IFavoriteProductCase favoriteProductCase;
-        private readonly ISearchFavoritesCase searchFavoritesCase;
-        private readonly ICountFavoritesCase countFavoritesCase;
 
         public ProductController(
             IGetProductInfoCase getProductInfoCase,
             IGetAllProductsByCategoriesCase getAllProductsByCategoriesCase,
-            IGetAllProductsBestSellersByCategoriesCase getAllProductsBestSellersByCategoriesCase,
-            IFavoriteProductCase favoriteProductCase,
-            ISearchFavoritesCase searchFavoritesCase,
-            ICountFavoritesCase countFavoritesCase
+            IGetAllProductsBestSellersByCategoriesCase getAllProductsBestSellersByCategoriesCase
         )
         {
             this.getProductInfoCase = getProductInfoCase;
             this.getAllProductsByCategoriesCase = getAllProductsByCategoriesCase;
             this.getAllProductsBestSellersByCategoriesCase = getAllProductsBestSellersByCategoriesCase;
-            this.favoriteProductCase = favoriteProductCase;
-            this.searchFavoritesCase = searchFavoritesCase;
-            this.countFavoritesCase = countFavoritesCase;
         }
 
         [HttpGet]
@@ -92,68 +83,6 @@ namespace API.Presenters.Controllers
             {
                 IEnumerable<GetAllProductsBestSellersResponse> response = getAllProductsBestSellersByCategoriesCase.Execute(request.Categories, request.Search, request.Limit, userId);
                 return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-        }
-
-        [HttpPut]
-        [Route("Favorite")]
-        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public IActionResult UpdateFavorite(UpdateFavoriteRequest request)
-        {
-            int userId = 1; // TODO: Remove for Token
-
-            try
-            {
-                bool newValue = favoriteProductCase.Execute(userId, request.ProductId);
-                return Ok(newValue);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-        }
-
-        [HttpPost]
-        [Route("SearchFavorites")]
-        [ProducesResponseType(typeof(IEnumerable<SearchFavoritesResponse>), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public IActionResult SearchFavorites(SearchFavoritesRequest request)
-        {
-            int userId = 1;
-
-            try
-            {
-                IEnumerable<SearchFavoritesResponse> response = searchFavoritesCase.Execute(userId, request.Search);
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-        }
-
-        [HttpGet]
-        [Route("CountFavorites")]
-        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public IActionResult GetCountFavorites()
-        {
-            int userId = 1;
-
-            try
-            {
-                int count = countFavoritesCase.Execute(userId);
-                return Ok(count);
             }
             catch (Exception ex)
             {
